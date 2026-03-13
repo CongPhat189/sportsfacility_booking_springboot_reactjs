05ad50c934a6c9e986668d57400ee2cad59c7efb
Công Phát Trần <tpn18092004@gmail.com>
2026-03-13 13:45:58 +0700

    done API Category Management

---
+++ sportsfacility_backend/src/main/java/com/example/sportsfacility_backend/repository/CourtCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.example.sportsfacility_backend.repository;
+
+import com.example.sportsfacility_backend.entity.CourtCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CourtCategoryRepository extends JpaRepository<CourtCategory, Integer> {
+
+    Optional<CourtCategory> findByName(String name);
+
+}
